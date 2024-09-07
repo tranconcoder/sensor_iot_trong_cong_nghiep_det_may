96298862a618a735b8cddb9229fcdc47900bd420
tvconss <tranconcoder@gmail.com>
2024-09-07 21:00:27 +0700

    optimize npm command

--- nodejs/src/routes/views.route.ts
+++ nodejs/src/routes/views.route.ts
@@ -8,5 +8,8 @@ viewsRouter.get("/stream-cam-ffmpeg", (_, res) => {
 viewsRouter.get("/stream-cam-flv", (_, res) => {
     res.render("pages/stream-cam-flv");
 });
+viewsRouter.get("/home", (_, res) => {
+    res.render("pages/home-page");
+});
 
 export default viewsRouter;
